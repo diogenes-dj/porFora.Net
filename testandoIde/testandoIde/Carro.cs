f48f52e2536699ca8f3a608cd4a3fc1adf02e8ec
diogenes-dj <zyguifried@hotmail.com>
2023-05-04 02:17:00 -0300

    ExerciciosIniciais

---
+++ testandoIde/testandoIde/Carro.cs
@@ -0,0 +1,26 @@
+﻿namespace testandoIde
+{
+    internal class Carro
+    {
+        //As propriedades são sempre substantivos
+        public string Cor { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int Ano { get; set; }
+
+        public bool UnicoDono { get; set; }
+
+        //Métodos são sempre verbos pq eles definem o q a classe pode fazer
+       
+        public void Ligar()
+        {
+            Console.WriteLine("BrBrBrBr");
+        }
+
+        public void Buzinar()
+        {
+            Console.WriteLine("BiiiiiBiiiii");
+        }
+    }
+}
---
+++ testandoIde/testandoIde/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using testandoIde;
+
+var carros = new List<Carro>();
+
+carros.Add(new Carro
+{
+    Ano = 2014,
+    Cor = "Azul",
+    Modelo = "Corolla",
+    UnicoDono = false 
+});
+
+carros.Add(new Carro
+{
+    Ano = 2022,
+    Cor = "Preto",
+    Modelo = "Camaro",
+    UnicoDono = true
+});
+
+carros.Add(new Carro
+{
+    Ano = 1968,
+    Cor = "Amarelo",
+    Modelo = "Corsa",
+    UnicoDono = false
+});
+
+Console.WriteLine("Veja os nossos Carros a venda");
+Console.WriteLine("Descreva seu modelo");
+
+foreach (var carro in carros)  //vai rodar pra cada carro q tiver na lista de carros 
+{
+    Console.WriteLine(carro.Ano);
+    Console.WriteLine(carro.Cor);
+    Console.WriteLine(carro.Modelo);
+    Console.WriteLine(carro.UnicoDono);
+
+}
+carros.FirstOrDefault(carro => carro.Modelo == "Camaro");
+
+/*
+ 1 - Fazer a lista ficar bonita
+ 2 - Opção pra escolha do carro
+ 3 - Mostrar as ações do carro
+ 4 - executar a ação escolhida
+ 
+ */
